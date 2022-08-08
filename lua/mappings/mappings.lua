138f342d15167c68d502d2801d5afc5c8e8e4afb
R0L0ct <facundoalem12@gmail.com>
2022-08-08 12:26:29 -0300

    mappingMod

--- lua/mappings/mappings.lua
+++ lua/mappings/mappings.lua
@@ -1,6 +1,8 @@
 local opts = {noremap = true, silent = true}
 local keymap = vim.api.nvim_set_keymap
 
+local term_opts = { silent = true }
+
 vim.g.mapleader = ' '
 
 -- Normal --
@@ -17,4 +19,27 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 --Lexplore
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
+--Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+-- Visual Block --
+-- Move text up and down --
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Visual --
+-- Stay in indent mode --
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Terminal --
+-- Better termianl navigation --
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
