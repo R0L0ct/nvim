1ec126d927ba53adb631fe56dafae12c8102a98a
R0L0ct <facundoalem12@gmail.com>
2022-09-20 19:09:46 -0300

    add jest and mocha debugger

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -32,7 +32,36 @@ for _, language in ipairs({ "typescript", "javascript" }) do
 			name = "Attach",
 			processId = require'dap.utils'.pick_process,
 			cwd = "${workspaceFolder}",
-		}
+		},
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Debug Jest Tests",
+			-- trace = true, -- include debugger info
+			runtimeExecutable = "node",
+			runtimeArgs = {
+				"./node_modules/jest/bin/jest.js",
+				"--runInBand",
+			},
+			rootPath = "${workspaceFolder}",
+			cwd = "${workspaceFolder}",
+			console = "integratedTerminal",
+			internalConsoleOptions = "neverOpen",
+  	},
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Debug Mocha Tests",
+			-- trace = true, -- include debugger info
+			runtimeExecutable = "node",
+			runtimeArgs = {
+				"./node_modules/mocha/bin/mocha.js",
+			},
+			rootPath = "${workspaceFolder}",
+			cwd = "${workspaceFolder}",
+			console = "integratedTerminal",
+			internalConsoleOptions = "neverOpen",
+  	}
   }
 end
 
